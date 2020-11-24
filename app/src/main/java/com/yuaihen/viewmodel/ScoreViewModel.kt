21114e22c56cdb0c41152e7e9e05613e7b239d41
Yuaihen <706375160@qq.com>
2020-11-24 19:06:17 +0800

    add:计分器

---
+++ app/src/main/java/com/yuaihen/viewmodel/ScoreViewModel.kt
@@ -0,0 +1,47 @@
+package com.yuaihen.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+/**
+ * Created by Yuaihen.
+ * on 2020/11/24
+ */
+class ScoreViewModel : ViewModel() {
+
+    var scoreTeamA = MutableLiveData<Int>()
+    var scoreTeamB = MutableLiveData<Int>()
+    private var aBack = 0
+    private var bBack = 0
+
+    init {
+        scoreTeamA.value = 0
+        scoreTeamB.value = 0
+    }
+
+    fun addATeam(n: Int) {
+        aBack = scoreTeamA.value!!
+        bBack = scoreTeamB.value!!
+        scoreTeamA.value = scoreTeamA.value?.plus(n)
+    }
+
+
+    fun addBTeam(n: Int) {
+        aBack = scoreTeamA.value!!
+        bBack = scoreTeamB.value!!
+        scoreTeamB.value = scoreTeamB.value?.plus(n)
+    }
+
+    fun reset() {
+        aBack = scoreTeamA.value!!
+        bBack = scoreTeamB.value!!
+        scoreTeamA.value = 0
+        scoreTeamB.value = 0
+    }
+
+    fun undo() {
+        scoreTeamA.value = aBack
+        scoreTeamB.value = bBack
+    }
+
+}
