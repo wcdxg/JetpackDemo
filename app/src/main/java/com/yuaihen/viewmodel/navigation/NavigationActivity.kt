3fe8589b07f5d40bebbf9eb5f6927eb99d69ddcb
Yuaihen <706375160@qq.com>
2020-11-30 18:32:41 +0800

    add:NavigationUI 设置返回箭头标题

--- app/src/main/java/com/yuaihen/viewmodel/navigation/NavigationActivity.kt
+++ app/src/main/java/com/yuaihen/viewmodel/navigation/NavigationActivity.kt
@@ -2,12 +2,24 @@ package com.yuaihen.viewmodel.navigation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.yuaihen.viewmodel.R
 
 class NavigationActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navigation)
+        navController = Navigation.findNavController(this, R.id.fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
+//        return super.onSupportNavigateUp()
     }
 }
