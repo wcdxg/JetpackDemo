dab83ace629915cce315ce61e6d1e73e577ff9bb
Yuaihen <706375160@qq.com>
2020-12-07 11:36:53 +0800

    add:优化设置点击事件时候的性能

--- app/src/main/java/com/yuaihen/viewmodel/room/RoomAdapter.kt
+++ app/src/main/java/com/yuaihen/viewmodel/room/RoomAdapter.kt
@@ -22,33 +22,16 @@ class RoomAdapter(private val useCardView: Boolean, val viewModel: WordViewModel
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        return MyViewHolder(
+        val holder = MyViewHolder(
             LayoutInflater.from(parent.context).inflate(
                 if (useCardView) R.layout.item_room_card else R.layout.item_room_normal,
                 parent,
                 false
             )
         )
-    }
-
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val word = data[position]
-        holder.itemView.indexText.text = position.plus(1).toString()
-        holder.itemView.englishText.text = word.englishWord
-        holder.itemView.chineseText.text = word.chineseMeaning
-
-        //需要先设置为null 避免item复用导致显示状态错误
-        holder.itemView.chineseSwitch.setOnCheckedChangeListener(null)
-
-        if (word.chineseInvisible) {
-            holder.itemView.chineseText.visibility = View.GONE
-            holder.itemView.chineseSwitch.isChecked = true
-        } else {
-            holder.itemView.chineseText.visibility = View.VISIBLE
-            holder.itemView.chineseSwitch.isChecked = false
-        }
 
         holder.itemView.chineseSwitch.setOnCheckedChangeListener { _, isChecked ->
+            val word = holder.itemView.getTag(R.id.word_for_view_holder) as Word
             if (isChecked) {
                 holder.itemView.chineseText.visibility = View.GONE
                 word.chineseInvisible = true
@@ -60,8 +43,10 @@ class RoomAdapter(private val useCardView: Boolean, val viewModel: WordViewModel
             }
         }
 
+
         holder.itemView.rootView.setOnClickListener {
-            val uri = Uri.parse("http://m.youdao.com/dict?le=eng&q=${data[position].englishWord}")
+            val uri =
+                Uri.parse("http://m.youdao.com/dict?le=eng&q=${holder.itemView.englishText.text}")
             val intent = Intent().apply {
                 action = Intent.ACTION_VIEW
                 data = uri
@@ -69,6 +54,50 @@ class RoomAdapter(private val useCardView: Boolean, val viewModel: WordViewModel
             holder.itemView.context.startActivity(intent)
 
         }
+
+        return holder
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val word = data[position]
+        holder.itemView.setTag(R.id.word_for_view_holder, word)
+        holder.itemView.indexText.text = position.plus(1).toString()
+        holder.itemView.englishText.text = word.englishWord
+        holder.itemView.chineseText.text = word.chineseMeaning
+
+        //需要先设置为null 避免item复用导致显示状态错误
+//        holder.itemView.chineseSwitch.setOnCheckedChangeListener(null)
+
+        if (word.chineseInvisible) {
+            holder.itemView.chineseText.visibility = View.GONE
+            holder.itemView.chineseSwitch.isChecked = true
+        } else {
+            holder.itemView.chineseText.visibility = View.VISIBLE
+            holder.itemView.chineseSwitch.isChecked = false
+        }
+
+        //在OnCreateViewHolder时设置监听 避免在滚动时多次创建消耗
+//        holder.itemView.chineseSwitch.setOnCheckedChangeListener { _, isChecked ->
+//            if (isChecked) {
+//                holder.itemView.chineseText.visibility = View.GONE
+//                word.chineseInvisible = true
+//                viewModel.wordRepository.updateWords(word)
+//            } else {
+//                holder.itemView.chineseText.visibility = View.VISIBLE
+//                word.chineseInvisible = false
+//                viewModel.wordRepository.updateWords(word)
+//            }
+//        }
+//
+//        holder.itemView.rootView.setOnClickListener {
+//            val uri = Uri.parse("http://m.youdao.com/dict?le=eng&q=${data[position].englishWord}")
+//            val intent = Intent().apply {
+//                action = Intent.ACTION_VIEW
+//                data = uri
+//            }
+//            holder.itemView.context.startActivity(intent)
+//
+//        }
     }
 
     override fun getItemCount(): Int {
